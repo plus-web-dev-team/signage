173a63982f7af3b69ab75d7ebb414ac843482c89
plus-agc <yamauchi-t@plus-agc.com>
2025-03-05 09:36:48 +0900

    modified

--- bs-config.js
+++ bs-config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  server: './', // ルートディレクトリ
-  files: ['**/*.html', 'css/**/*.css', 'js/**/*.js'], // 監視するファイル
+	server: "./", // ルートディレクトリ
+	files: ["**/*.html", "css/**/*.css", "js/**/*.js"], // 監視するファイル
+	// ブラウザの選択
+	browser: "google chrome",
 };
